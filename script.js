a1d74eebc01ea79a4a1eeeda6286278e04a5f243
Jovanny <jovannyrebollar1@gmail.com>
2022-03-13 21:43:24 -0400

    Working on matching password logic

---
+++ script.js
@@ -0,0 +1,14 @@
+const password1 = document.querySelector('#password');
+const password2 = document.querySelector('#confrmPassword');
+
+// TODO when the create button is clicked will check if the passwords are similar or not.
+
+function checkPW() {
+    if (password1.value === password2.value) {
+        password1.classList.toggle('correct');
+    }
+}
+
+
+
+
