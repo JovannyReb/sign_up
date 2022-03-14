393923d7077553ecc0ad412fdd5cf609da45c70f
Jovanny <jovannyrebollar1@gmail.com>
2022-03-14 00:34:30 -0400

    JS logic for when sign up button is clicked checks for similar pw

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const password1 = document.querySelector('#password');
 const password2 = document.querySelector('#confrmPassword');
+const button = document.querySelector("#main-btn");
+const text = document.querySelector('.error-text');
 
 // TODO when the create button is clicked will check if the passwords are similar or not.
 
 function checkPW() {
     if (password1.value === password2.value) {
-        password1.classList.toggle('correct');
-        password2.classList.toggle('correct');
+        console.log("Works")
+        password1.style.borderColor = "green";
+        password2.style.borderColor = "green";
+        password1.style.borderWidth = "2px";
+        password2.style.borderWidth = "2px";
+        text.style.visibility = "hidden";
+    }
+    else {
+        password1.style.borderColor = "red";
+        password2.style.borderColor = "red";
+        password1.style.borderWidth = "2px";
+        password2.style.borderWidth = "2px";
+        text.style.visibility = "visible";
     }
 }
+
+
+button.addEventListener('focus', checkPW );
 
 
 
